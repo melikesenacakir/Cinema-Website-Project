70e96cc81acf89278d6c7b1c2544df0a310edac5
iamsinem <114025826+iamsinem@users.noreply.github.com>
2023-06-04 17:02:43 +0300

    Add files via upload

--- project/index.php
+++ project/index.php
@@ -6,8 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cinema MSB</title>
-    <link rel="stylesheet" href="bootstrap-5.3.0-alpha3-dist/css/bootstrap.css">
-    <script src="bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" href="bootstrap.css">
+    <script src="bootstrap.bundle.js"></script>
 
     <style>
         .card-img-top {
@@ -65,12 +65,12 @@
                         FİLM TÜRLERİ
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Aksiyon</a></li>
-                        <li><a class="dropdown-item" href="#">Romantik-Komedi</a></li>
-                        <li><a class="dropdown-item" href="#">Animasyon</a></li>
-                        <li><a class="dropdown-item" href="#">Biyografi</a></li>
-                        <li><a class="dropdown-item" href="#">Dram</a></li>
-                        <li><a class="dropdown-item" href="#">Korku</a></li>
+                        <li><a class="dropdown-item" href="aksiyon.php">Aksiyon</a></li>
+                        <li><a class="dropdown-item" href="romantik-komedi.php">Romantik-Komedi</a></li>
+                        <li><a class="dropdown-item" href="animasyon.php">Animasyon</a></li>
+                        <li><a class="dropdown-item" href="biyografi.php">Biyografi</a></li>
+                        <li><a class="dropdown-item" href="dram.php">Dram</a></li>
+                        <li><a class="dropdown-item" href="korku.php">Korku</a></li>
                     </ul>
                 </li>
 
@@ -83,10 +83,10 @@
             </form>
             <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
                 <div class="btn-group-sm me-2 mt-1" role="group" aria-label="First group">
-                    <button type="button" class="btn btn-outline-light">Oturum aç</button>
+                    <button type="button" class="btn btn-outline-secondary" ><a href="login.php" style="text-decoration:none" class="text-light">Oturum aç</a> </button>
                 </div>
                 <div class="btn-group-sm me-2 mt-1" role="group" aria-label="Second group">
-                    <button type="button" class="btn btn-outline-light">Kayıt ol</button>
+                    <button type="button" class="btn btn-outline-secondary"><a href="kayit.php" style="text-decoration:none" class="text-light">Kayıt ol</a> </button>
                 </div>
             </div>
         </div>
@@ -106,7 +106,7 @@
             <img src="Resimler/sale.png" class="d-block mx-auto img-fluid" style="height: 500px;" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="Resimler/YAKINDA%20VİZYONDA!.png" class="d-block mx-auto img-fluid" style="height: 500px;" alt="...">
+            <img src="Resimler/YAKINDAVİZYONDA!.png" class="d-block mx-auto img-fluid" style="height: 500px;" alt="...">
         </div>
     </div>
     <button class="carousel-control-prev bg-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -122,102 +122,137 @@
 
 <div>
     <!-- container start!-->
-    <div class="container mt-4">
+    <div  id="vizyondakifilmler" class="container mt-4">
         <div class="row">
             <div class="col-sm-3 mt-4">
-                <div class="card custom-card" >
-                    <img src="Resimler/hizliveofkeli10.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">HIZLI VE ÖFKELİ 10</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="hizliveofkeli10.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/hizliveofkeli10.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">HIZLI VE ÖFKELİ 10</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="col-sm-3 mt-4">
-                <div class="card custom-card" >
-                    <img src="Resimler/galaksininkoruyuculari3.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">GALAKSİNİN KORUYUCULARI</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="galaksininkoruyuculari3.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/galaksininkoruyuculari3.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">GALAKSİNİN KORUYUCULARI</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="col-sm-3 mt-4">
-                <div class="card custom-card" >
-                    <img src="Resimler/araf-6-olum.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">ARAF 6:ÖLÜM</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="araf-6-olum.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/araf-6-olum.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">ARAF 6:ÖLÜM</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="col-sm-3 mt-4">
-                <div class="card custom-card" >
-                    <img src="Resimler/karanlikgece.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">KARANLIK GECE</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="karanlikgece.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/karanlikgece.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">KARANLIK GECE</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="col-sm-3 mt-5">
-                <div class="card custom-card" >
-                    <img src="Resimler/neseliciftliktrenkalkiyor.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">NEŞELİ ÇİFTLİK:TREN KALKIYOR</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="neseliciftliktrenkalkiyor.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/neseliciftliktrenkalkiyor.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">NEŞELİ ÇİFTLİK:TREN KALKIYOR</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="col-sm-3 mt-5">
-                <div class="card custom-card" >
-                    <img src="Resimler/kurak-gunler-.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">KURAK GÜNLER</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="kurak-gunler.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/kurak-gunler-.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">KURAK GÜNLER</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="col-sm-3 mt-5">
-                <div class="card custom-card" >
-                    <img src="Resimler/mannucanakkalede.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">MANNU ÇANAKKALEDE</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="mannucanakkalede.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/mannucanakkalede.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">MANNU ÇANAKKALEDE</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="col-sm-3 mt-5">
-                <div class="card custom-card" >
-                    <img src="Resimler/kotu-ruh-uyanis.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">KÖTÜ RUH:UYANIŞ</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="kotu-ruh-uyanis.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/kotu-ruh-uyanis.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">KÖTÜ RUH:UYANIŞ</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="col-sm-3 mt-5">
-                <div class="card custom-card" >
-                    <img src="Resimler/askin-bununla-ne-ilgisi-var-.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">AŞKIN BUNUNLA NE İLGİSİ VAR?</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="askin-bununla-ne-ilgisi-var.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/askin-bununla-ne-ilgisi-var-.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">AŞKIN BUNUNLA NE İLGİSİ VAR?</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="col-sm-3 mt-5">
-                <div class="card custom-card" >
-                    <img src="Resimler/resmi-yarisma-.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">RESMİ YARIŞMA</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="resmi-yarisma.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/resmi-yarisma-.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">RESMİ YARIŞMA</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="col-sm-3 mt-5">
-                <div class="card custom-card" >
-                    <img src="Resimler/dali-diyari.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">DALİ DİYARI</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="dali-diyari.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/dali-diyari.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">DALİ DİYARI</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="col-sm-3 mt-5">
-                <div class="card custom-card" >
-                    <img src="Resimler/korsaj-.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text" style="font-size: larger; font-weight: bold;">KORSAJ</p>
-                    </div>
+                <div class="card custom-card">
+                    <a href="korsaj.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                        <img src="Resimler/korsaj-.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">KORSAJ</p>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
@@ -234,9 +269,8 @@
                 <div class="col-md-2">
                     <h6><br></h6>
                     <ul class="list-unstyled">
-                        <li><a style="color: #ced4da" href="#">Anasayfa</a></li>
-                        <li><a style="color: #ced4da" href="#">Filmler</a></li>
-                        <li><a style="color: #ced4da" href="#">Vizyondakiler</a></li>
+                        <li><a style="color: #ced4da" href="index.php">Anasayfa</a></li>
+                        <li><a style="color: #ced4da" href="#vizyondakifilmler">Vizyondaki Filmler</a></li>
 
 
                     </ul>
@@ -244,9 +278,9 @@
                 <div class="col-md-3">
                     <h6><br></h6>
                     <ul class="list-unstyled">
-                        <li><a style="color: #ced4da"" href="#">KVK Aydınlatma Bildirimi</a></li>
-                        <li><a style="color: #ced4da" href="#">KVK Politikasi</a></li>
-                        <li><a style="color: #ced4da" href="#">Hakkımızda</a></li>
+                        <li><a style="color: #ced4da" href="kvkaydınlatma.php">KVK Aydınlatma Bildirimi</a></li>
+                        <li><a style="color: #ced4da" href="kvkpolitika.php">KVK Politikasi</a></li>
+
                     </ul>
                 </div>
                 <div class="col-md-3">
@@ -262,11 +296,9 @@
                 <div class="col-md-2" >
                     <h6><br></h6>
                     <h6><b>SOSYAL MEDYA</b></h6>
-                    <a href="#"><i class="fa fa-instagram mx-md-1"></i><img style="background-color: white" class="rounded-5" src="Resimler/instagram.png" width="20" height="20"></a>
-                    <a href="#"><i class="fa fa-facebook mx-md-1"></i><img style="background-color: white" class="rounded-5" src="Resimler/facebook.png"  width="20" height="20"></a>
-                    <a href="#"><i class="fa fa-twitter mx-md-1"></i><img style="background-color: white" class="rounded-5" src="Resimler/twitter.png" width="20" height="20"></a>
-
-
+                    <a href="https://www.secure.instagram.com/accounts/login/"><i class="fa fa-instagram mx-md-1"></i><img style="background-color: white" class="rounded-5" src="Resimler/instagram.png" width="20" height="20"></a>
+                    <a href="https://tr-tr.facebook.com/"><i class="fa fa-facebook mx-md-1"></i><img style="background-color: white" class="rounded-5" src="Resimler/facebook.png"  width="20" height="20"></a>
+                    <a href="https://twitter.com/"><i class="fa fa-twitter mx-md-1"></i><img style="background-color: white" class="rounded-5" src="Resimler/twitter.png" width="20" height="20"></a>
                 </div>
                 <div class="clearfix"></div>
                 <p class="mt-5 float-right">© CinemaMSB 2023 </p>
