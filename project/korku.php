1f5f468093686550e87a9809f9e1db749a53d389
iamsinem <114025826+iamsinem@users.noreply.github.com>
2023-06-04 21:47:59 +0300

    Update korku.php

--- project/korku.php
+++ project/korku.php
@@ -96,7 +96,7 @@
         <div class="row p-md-5 min-vw-50 ">
             <div class="col col-sm-auto ">
                 <div class="card custom-card" >
-                    <a href="araf-6-olum.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                    <a href="Filmler/araf-6-olum.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
                         <img src="Resimler/araf-6-olum.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">ARAF 6:ÖLÜM</p>
@@ -106,7 +106,7 @@
             </div>
             <div class="col  col-sm-auto">
                 <div class="card custom-card mt-auto" >
-                    <a href="kotu-ruh-uyanis.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
+                    <a href="Filmler/kotu-ruh-uyanis.php" style="text-decoration: none;"> <!-- Update the href attribute to the PHP page -->
                         <img src="Resimler/kotu-ruh-uyanis.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <p class="card-text" style="font-size: larger; font-weight: bold; color: black;">KÖTÜ RUH:UYANIŞ</p>
