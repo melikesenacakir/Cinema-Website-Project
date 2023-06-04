308ad3306ff8e35fa368d3b5c3e227a5749ce513
bbernatann <122397159+bbernatann@users.noreply.github.com>
2023-06-04 15:45:58 +0300

    Add files via upload

---
+++ project/Filmler/korsaj.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cinema MSB</title>
+    <link rel="stylesheet" href="../bootstrap.css">
+    <script src="../bootstrap.bundle.js"></script>
+
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+    <div class="container-fluid">
+        <div class="row mb-0 me-2">
+            <div class="col mb-0">
+                <img src="../Resimler/amblem.png" style="height: 50px" class="img-fluid" alt="..."></img>
+            </div>
+        </div>
+        <span class="navbar-brand mb-0 h1">CinemaMSB</span>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        MENÜ
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Bilet İşlemleri</a></li>
+                        <li><a class="dropdown-item" href="#">Öneri Ve Şikayet</a></li>
+                        <li><a class="dropdown-item" href="#">İletişim</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        FİLM TÜRLERİ
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Aksiyon</a></li>
+                        <li><a class="dropdown-item" href="#">Romantik-Komedi</a></li>
+                        <li><a class="dropdown-item" href="#">Animasyon</a></li>
+                        <li><a class="dropdown-item" href="#">Biyografi</a></li>
+                        <li><a class="dropdown-item" href="#">Dram</a></li>
+                        <li><a class="dropdown-item" href="#">Korku</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <form class="d-flex me-2" role="search">
+                <button class="btn btn-light text-dark btn-sm rounded-start-pill" type="submit">
+                    <img src="../Resimler/arama.png" class="img-fluid" style="height: 30px;">
+                </button>
+                <input class="form-control me-auto rounded-end-pill" type="search"  placeholder="aradığınız film nedir?" aria-label="Search">
+            </form>
+            <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                <div class="btn-group-sm me-2 mt-1" role="group" aria-label="First group">
+                    <button type="button" class="btn btn-outline-light">Oturum aç</button>
+                </div>
+                <div class="btn-group-sm me-2 mt-1" role="group" aria-label="Second group">
+                    <button type="button" class="btn btn-outline-light">Kayıt ol</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+<div class="card" style="">
+    <div class="row no-gutters">
+        <div class="col-md-4">
+            <img src="../Resimler/korsaj-.jpg" class="card-img" alt="..." style="width: 400px; height: 450px;">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <h1 class="card-title">KORSAJ</h1>
+                    <h6 class="card-text">15 Mayıs 2023 Pazartesi</h6>
+                    <h6 class="card-text">Yönetmen: Marie Kreutzer</h6>
+                    <h6 class="card-text">Oyuncular: Vicky Krieps, Colin Morgan, Florian Teichtmeister</h6>
+                    <h6 class="card-text">Süre:113 dakika</h6>
+                    <a href="#"><i class="fa fa-facebook mx-md-1"></i><img style="background-color: white" class="rounded-5" src="../Resimler/olumsuz-rnek-20162592249.png" width="20" height="20"></a>
+                    <a href="#"><i class="fa fa-twitter mx-md-1"></i><img style="background-color: white" class="rounded-5" src="../Resimler/13-2016811111627.png" width="20" height="20"></a>
+                </div>
+                <div class="align-self-end">
+                    <a href="#" class="btn btn-primary">Biletini Al</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container mt-4">
+    <div class="row">
+        <div class="">
+            <div class="">
+                <h3 class="card-subtitle mb-2">ÖZET:</h3>
+                <h5 class="card-text">Söylendiğine göre Bavyera Düşesi Elisabeth’in bel ölçüsü yıllar boyu yalnızca 45 cm olarak kalmış. Fakat Sisi olarak da bilinen Avusturya İmparatoriçesi Elisabeth, yalnızca belini böyle ince özel yapım korseleri yüzünden değil, Viyana’daki sarayda kendine biçilen sessiz ve işlevsiz biblo rolü yüzünden de boğulmuş, nefessiz kalmış. Dünya prömiyerini 2022 Cannes’da Belirli Bir Bakış bölümünde yapan Korsaj, Elisabeth’in 40. doğum günü kutlamalarını dönüm noktası alarak sonunda zincirlerini kırmaya niyetlenen, bilgiye ve yaşama aç bir kadının isyanını gözler önüne seriyor. Kırılgan olduğu kadar öfkeli, narin ama dişli İmparatoriçe’yi kusursuz canlandıran Vicky Krieps, performansıyla Cannes’da ödül kazandı. Korsaj, Avusturya’nın Oscar adayı oldu. Yönetmen Marie Kreutzer’in yine baskı altındaki bir kadını merkezine alan Der Boden unter den Füssen / Kaygan Zemin filmi 2019’da İstanbul Film Festivali’nde gösterilmişti.</h5>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<footer class="bg-dark"  >
+    <div class="container text-white mt-5">
+        <div class="row">
+            <div class="col-md-2">
+                <h3><b>CinemaMSB</b></h3>
+                <img src="../Resimler/amblem.png " width="150" height="100">
+            </div>
+            <div class="col-md-2">
+                <h6><br></h6>
+                <ul class="list-unstyled">
+                    <li><a style="color: #ced4da" href="#">Anasayfa</a></li>
+
+                    <li><a style="color: #ced4da" href="#vizyondaki-filmler">Vizyondaki Filmler</a></li>
+
+
+
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h6><br></h6>
+                <ul class="list-unstyled">
+                    <li><a style="color: #ced4da"" href="#">KVK Aydınlatma Bildirimi</a></li>
+                    <li><a style="color: #ced4da" href="#">KVK Politikasi</a></li>
+                    <li><a style="color: #ced4da" href="#">Hakkımızda</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h6><br></h6>
+                <h6><b>İLETİŞİM BİLGİLERİ</b></h6>
+                <ul class="list-unstyled">
+                    <li><i style="color: #41464b class="fa fa-map-marker"></i>Adres: Karabük, Merkez Kares AVM</li>
+                    <li><i style="color: #41464b class="fa fa-envelope"></i> Email:cinemamsb@example.com</li>
+                    <li><i style="color: #41464b class="fa fa-phone"></i> Telefon: +1234567890</li>
+                </ul>
+
+            </div>
+            <div class="col-md-2" >
+                <h6><br></h6>
+                <h6><b>SOSYAL MEDYA</b></h6>
+                <a href="#"><i class="fa fa-instagram mx-md-1"></i><img style="background-color: white" class="rounded-5" src="../Resimler/instagram.png" width="20" height="20"></a>
+                <a href="#"><i class="fa fa-facebook mx-md-1"></i><img style="background-color: white" class="rounded-5" src="../Resimler/facebook.png" width="20" height="20"></a>
+                <a href="#"><i class="fa fa-twitter mx-md-1"></i><img style="background-color: white" class="rounded-5" src="../Resimler/twitter.png" width="20" height="20"></a>
+
+
+            </div>
+            <div class="clearfix"></div>
+            <p class="mt-5 float-right">© CinemaMSB 2023 </p>
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ project/Filmler/kotu-ruh-uyanis.php
@@ -0,0 +1,164 @@
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cinema MSB</title>
+    <link rel="stylesheet" href="../bootstrap.css">
+    <script src="../bootstrap.bundle.js"></script>
+
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+    <div class="container-fluid">
+        <div class="row mb-0 me-2">
+            <div class="col mb-0">
+                <img src="../Resimler/amblem.png" style="height: 50px" class="img-fluid" alt="..."></img>
+            </div>
+        </div>
+        <span class="navbar-brand mb-0 h1">CinemaMSB</span>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        MENÜ
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Bilet İşlemleri</a></li>
+                        <li><a class="dropdown-item" href="#">Öneri Ve Şikayet</a></li>
+                        <li><a class="dropdown-item" href="#">İletişim</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        FİLM TÜRLERİ
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Aksiyon</a></li>
+                        <li><a class="dropdown-item" href="#">Romantik-Komedi</a></li>
+                        <li><a class="dropdown-item" href="#">Animasyon</a></li>
+                        <li><a class="dropdown-item" href="#">Biyografi</a></li>
+                        <li><a class="dropdown-item" href="#">Dram</a></li>
+                        <li><a class="dropdown-item" href="#">Korku</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <form class="d-flex me-2" role="search">
+                <button class="btn btn-light text-dark btn-sm rounded-start-pill" type="submit">
+                    <img src="../Resimler/arama.png" class="img-fluid" style="height: 30px;">
+                </button>
+                <input class="form-control me-auto rounded-end-pill" type="search"  placeholder="aradığınız film nedir?" aria-label="Search">
+            </form>
+            <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                <div class="btn-group-sm me-2 mt-1" role="group" aria-label="First group">
+                    <button type="button" class="btn btn-outline-light">Oturum aç</button>
+                </div>
+                <div class="btn-group-sm me-2 mt-1" role="group" aria-label="Second group">
+                    <button type="button" class="btn btn-outline-light">Kayıt ol</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+<div class="card" style="">
+    <div class="row no-gutters">
+        <div class="col-md-4">
+            <img src="../Resimler/kotu-ruh-uyanis.jpg" class="card-img" alt="..." style="width: 400px; height: 450px;">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <h1 class="card-title">KÖTÜ RUH:UYANIŞ</h1>
+                    <h6 class="card-text">21 Nisan 2023 Cuma</h6>
+                    <h6 class="card-text">Yönetmen: Lee Coronin</h6>
+                    <h6 class="card-text">Oyuncular: Lily Sullivan, Alyssa Sutherland</h6>
+                    <h6 class="card-text">Süre:97 dakika</h6>
+                    <a href="#"><i class="fa fa-instagram mx-md-1"></i><img style="background-color: white" class="rounded-5" src="../Resimler/şiddet-ve-korku-2016811111844.png" width="20" height="20"></a>
+                    <a href="#"><i class="fa fa-facebook mx-md-1"></i><img style="background-color: white" class="rounded-5" src="../Resimler/olumsuz-rnek-20162592249.png" width="20" height="20"></a>
+                    <a href="#"><i class="fa fa-twitter mx-md-1"></i><img style="background-color: white" class="rounded-5" src="../Resimler/16.png" width="20" height="20"></a>
+                </div>
+                <div class="align-self-end">
+                    <a href="#" class="btn btn-primary">Biletini Al</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container mt-4">
+    <div class="row">
+        <div class="">
+            <div class="">
+                <h3 class="card-subtitle mb-2">ÖZET:</h3>
+                <h5 class="card-text">Kötü Ruh: Uyanış, yıllar içinde birbirlerinden uzaklaşan iki kardeşin hayatına odaklanıyor. Zamanla birbirlerinden uzaklaşan iki kardeş, aradan geçen yılların ardından yeniden bir araya gelir. Ancak büyük bir tehlike onları beklemektedir çünkü et yiyen kötü ruhlar canlanmıştır. Kendilerini bir anda büyük bir kabusun içinde bulan iki kardeş, hayatta kalabilmek için farklılıklarının üstesinden gelip, birlikte hareket etmek zorundadır.</h5>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<footer class="bg-dark"  >
+    <div class="container text-white mt-5">
+        <div class="row">
+            <div class="col-md-2">
+                <h3><b>CinemaMSB</b></h3>
+                <img src="../Resimler/amblem.png " width="150" height="100">
+            </div>
+            <div class="col-md-2">
+                <h6><br></h6>
+                <ul class="list-unstyled">
+                    <li><a style="color: #ced4da" href="#">Anasayfa</a></li>
+
+                    <li><a style="color: #ced4da" href="#vizyondaki-filmler">Vizyondaki Filmler</a></li>
+
+
+
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h6><br></h6>
+                <ul class="list-unstyled">
+                    <li><a style="color: #ced4da"" href="#">KVK Aydınlatma Bildirimi</a></li>
+                    <li><a style="color: #ced4da" href="#">KVK Politikasi</a></li>
+                    <li><a style="color: #ced4da" href="#">Hakkımızda</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h6><br></h6>
+                <h6><b>İLETİŞİM BİLGİLERİ</b></h6>
+                <ul class="list-unstyled">
+                    <li><i style="color: #41464b class="fa fa-map-marker"></i>Adres: Karabük, Merkez Kares AVM</li>
+                    <li><i style="color: #41464b class="fa fa-envelope"></i> Email:cinemamsb@example.com</li>
+                    <li><i style="color: #41464b class="fa fa-phone"></i> Telefon: +1234567890</li>
+                </ul>
+
+            </div>
+            <div class="col-md-2" >
+                <h6><br></h6>
+                <h6><b>SOSYAL MEDYA</b></h6>
+                <a href="#"><i class="fa fa-instagram mx-md-1"></i><img style="background-color: white" class="rounded-5" src="../Resimler/instagram.png" width="20" height="20"></a>
+                <a href="#"><i class="fa fa-facebook mx-md-1"></i><img style="background-color: white" class="rounded-5" src="../Resimler/facebook.png" width="20" height="20"></a>
+                <a href="#"><i class="fa fa-twitter mx-md-1"></i><img style="background-color: white" class="rounded-5" src="../Resimler/twitter.png" width="20" height="20"></a>
+
+
+            </div>
+            <div class="clearfix"></div>
+            <p class="mt-5 float-right">© CinemaMSB 2023 </p>
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+
+
+</body>
+</html>
