f3b16c3a176f19df1b8d56a4b4cc5d4119d6187a
Melike Sena Çakır <115591143+melikesenacakir@users.noreply.github.com>
2023-06-04 22:16:04 +0300

    Update adminlogin.php

--- project/adminlogin.php
+++ project/adminlogin.php
@@ -6,8 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kullanıcı Giriş</title>
-    <link rel="stylesheet" href="bootstrap.css">
-    <script src="bootstrap.bundle.js"></script>
+    <link rel="stylesheet" href="bootstrap-5.3.0-alpha3-dist/css/bootstrap.css">
+    <script src="bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.js"></script>
 
 </head>
 <body>
